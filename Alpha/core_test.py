da6db1fcda5061a51c30cba9c86f943c0c938c9c
KKuette <awakeproduction@hotmail.fr>
2017-08-09 07:33:25 +0200

    Added datasets : DAX from 2k10 + EURUSD from 2k9 | Writed RNN.py | Multiple test runned, models in saved_models, README in for more info
    Former-commit-id: 4f1176b533823dda9f2ca2bf52c0464652db3e27
    Former-commit-id: adf89a79781f67cfdfadace0726dcedc556944fb

--- Alpha/core_test.py
+++ Alpha/core_test.py
@@ -14,24 +14,27 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.recurrent import LSTM
 from keras.models import load_model
 import os
+from interface import *
+from parsing import *
+from utils import *
 
                                 
 #Import csv file + traitement     
                                 
 
 def get_stock_data(stock_name, normalized=0):
-    location = './Datasets/Stock_price/googl1k.csv'
+    #location = './Datasets/Stock_price/googl1k.csv'
 
-    col_names = ['Date','Open','High','Low','Close','Volume']
-    stocks = pd.read_csv(location, header=0, names=col_names)
-    df = pd.DataFrame(stocks)
+    #col_names = ['Date','Open','High','Low','Close','Volume']
+    #stocks = pd.read_csv(location, header=0, names=col_names)
+    df = get_data()        #pd.DataFrame(stocks)
     date_split = df['Date'].str.split('-').str
     df['Day'], df['Month'], df['Yeah'] = date_split
     df["Volume"] = df["Volume"] / 10000
-    df.drop(df.columns[[0,3,5,6, 7, 8]], axis=1, inplace=True)
+    df.drop(df.columns[[3]], axis=1, inplace=True)
     return df
 
-stock_name = 'GOOGL'
+stock_name = 'EUR_USD'
 df = get_stock_data(stock_name,0)
 
 df['High'] = df['High'] / 100
@@ -40,14 +43,12 @@ df['Close'] = df['Close'] / 100
 #df['Low'] = df['Low'] / 100
 
 def load_data(stock, seq_len):
-    print(stock)
     amount_of_features = len(stock.columns)
     data = stock.as_matrix() #pd.DataFrame(stock)
     sequence_length = seq_len + 1
     result = []
     for index in range(len(data) - sequence_length):
         result.append(data[index: index + sequence_length])
-    #print(result)
     result = np.array(result)
     row = round(0.9 * result.shape[0])
     train = result[:int(row), :]
@@ -84,37 +85,42 @@ print("y_test", y_test.shape)
 #Training       
                 
 
-#saved_model = 'StockLSTM.HDF5'
+saved_model = 'Alpha_LSTM.HDF5'
 
-#if (os.path.isfile(saved_model)):
-#    model = load_model(saved_model)
-#else :
-#    model = build_model([3,window,1])
-#    open(saved_model, 'w')
+if (os.path.isfile(saved_model)):
+    model = load_model(saved_model)
+else :
+    model = build_model([3,window,1])
+    open(saved_model, 'w')
 
-#model.fit(  X_train, y_train, batch_size=512, epochs=100, validation_split=0.2, verbose=1)
+model.fit(  X_train,
+            y_train,
+            batch_size=512,
+            epochs=100,
+            validation_split=0.2,
+            verbose=1)
 
-#model.save(saved_model)
+model.save(saved_model)
 
                 
 #print score    
                 
 
-#trainScore = model.evaluate(X_train, y_train, verbose=0)
-#print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
+trainScore = model.evaluate(X_train, y_train, verbose=0)
+print('Train Score: %.2f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
 
-#testScore = model.evaluate(X_test, y_test, verbose=0)
-#print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
+testScore = model.evaluate(X_test, y_test, verbose=0)
+print('Test Score: %.2f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
 
-#diff=[]
-#ratio=[]
-#p = model.predict(X_test)
-#for u in range(len(y_test)):
-    #pr = p[u][0]
-    #ratio.append((y_test[u]/pr)-1)
-    #diff.append(abs(y_test[u]- pr))
+diff=[]
+ratio=[]
+p = model.predict(X_test)
+for u in range(len(y_test)):
+    pr = p[u][0]
+    ratio.append((y_test[u]/pr)-1)
+    diff.append(abs(y_test[u]- pr))
 
-#plt.plot(p,color='red', label='prediction')
-#plt.plot(y_test,color='blue', label='y_test')
-#plt.legend(loc='upper left')
-#plt.show()
+plt.plot(p,color='red', label='prediction')
+plt.plot(y_test,color='blue', label='y_test')
+plt.legend(loc='upper left')
+plt.show()
