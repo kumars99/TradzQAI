ed16390a49105ec95c493ac3479cbed865aa92f1
kkuette <awakeproduction@hotmail.fr>
2018-05-16 00:05:24 +0200

    Custom threaded logger.
    Former-commit-id: d51c73e3da4ceb4cfc7bb8be46acb317d67ecec1
    Former-commit-id: 726f7d72d87a278562d652c2a847678ad8afbb29
    Former-commit-id: 731dfbf984765108d491d052c794a7bfc8aef8ab

--- tools/logger.py
+++ tools/logger.py
@@ -1,59 +1,85 @@
-from .utils import str2bool
-
 import time
+import os
+from threading import Thread
 
-from collections import deque
-
-class Logger(object):
+class Logger(Thread):
 
     def __init__(self):
 
-        self.log_path = "logs/"
+        self.path = "logs/"
 
+        self.names = []
 
         self.logs = {}
         self.id = {}
         self.current_index = {}
 
-        self.conf = ""
-        self.new_logs(self.name)
-        self._add("Starting logs", self.name)
-        '''
-        self.logs[self.name].append(time.strftime("%Y:%m:%d %H:%M:%S") + \
-                        " " + '{:06d}'.format(self.id[self.name]) + " " \
-                        + str("Starting logs") + "\n")
+        self.log_file_name = {}
+        self.log_file = {}
+        self.log_path = ""
+        self.log_file_path = {}
 
-        self.id[self.name] += 1
-        '''
+        self._running = False
+
+        Thread.__init__(self)
 
     def new_logs(self, name):
         '''
-        name : name as string
+        name : log name as string
         '''
         if type(name) is str:
+            self.names.append(name)
             self.logs[name] = []
-            self.id[name] = 0
-            self.current_index[name] = 0
+            self.id[name] = -1
+            self.current_index[name] = -1
+            self.log_file_path[name] = self.log_path + self.check_log_file(name)
             self.log_file[name] = None
-            if self.files_checked is True:
-                self.load_log(name)
         else:
             raise ValueError("name should be a string")
 
-    def init_saver(self, env):
-        self._check(env.model_dir, self.log_path, env.settings)
-        self.load_log(self.name)
-        self._add("Saver initialized", self.name)
+    def set_log_path(self, base_path):
+        self.log_path = base_path + self.path
+        self.check_log_dir()
 
     def _add(self, log, name):
+        self.id[name] += 1
         self.logs[name].append(time.strftime("%Y:%m:%d %H:%M:%S") + \
             " " + '{:06d}'.format(self.id[name]) + " " + str(log) + "\n")
-        if self.log_file[name] != None:
+
+    def check_log_dir(self):
+        if not os.path.exists(self.log_path):
+            os.mkdir(self.log_path)
+
+    def check_log_file(self, log_name):
+        cdir = os.listdir(self.log_path)
+        for d in cdir:
+            if time.strftime("%Y_%m_%d") in d and log_name == d.split('_')[0]:
+                return d
+        return log_name + "_" + time.strftime("%Y_%m_%d") + ".txt"
+
+    def load_log(self, log_name):
+        self.log_file_name[log_name] = self.check_log_file(log_name)
+        self.log_file_path[log_name] = self.log_path + "/" + self.log_file_name[log_name]
+        self.log_file[log_name] = open(self.log_file_path[log_name], 'a')
+
+    def save_logs(self, logs=None, name=None):
+        self.log_file[name] = open(self.log_file_path[name], 'a')
+        self.log_file[name].write(logs)
+        self.log_file[name].close()
+
+    def write_logs(self):
+        for name in self.names:
             if self.current_index[name] < self.id[name]:
-                while self.current_index[name] <= self.id[name]:
-                    self._save(logs=self.logs[name][self.current_index[name]], logs_name=name)
+                while self.current_index[name] < self.id[name]:
                     self.current_index[name] += 1
-            else:
-                self._save(logs=self.logs[name][self.id[name]], logs_name=name)
-                self.current_index[name] += 1
-        self.id[name] += 1
+                    self.save_logs(logs=self.logs[name][self.current_index[name]], name=name)
+
+    def run(self, write_time=60):
+        self._running = True
+        while self._running:
+            self.write_logs()
+            tmp = 0
+            while tmp < write_time and self._running:
+                time.sleep(0.1)
+                tmp += 0.1
+        self.write_logs()
