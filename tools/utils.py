2aaedca176bc09ed1046b6804ab4fad9f15ed702
kkuette <awakeproduction@hotmail.fr>
2018-05-19 03:38:59 +0200

    getState now process variable columns
    Former-commit-id: a61c852d03b527af608976b4be4a4dd3af9a7ebd
    Former-commit-id: 90fd3b2748f16eb9786cb598eb939f543c03ed68
    Former-commit-id: 7f3366ad85b036557e3642e236e3124fe059f4e2

--- tools/utils.py
+++ tools/utils.py
@@ -46,20 +46,26 @@ def getStockDataVec(key):
             sep = ','
             len_row = len(lines.split(sep))
             if len_row == 4:
-                names = ['Time', 'BID', 'ASK', 'VOL']
+                #types = dict(BID='np.float64', ASK='np.float64', Volume='np.float64')
+                names = ['Time', 'BID', 'ASK', 'Volume']
             elif len_row == 3:
+                #types = dict(Price='np.float64', Volume='np.float64')
                 names = ['Time', 'Price', 'Volume']
+            elif len_row == 9:
+                names = ['Time', 'Price', 'Volume', 'RSI', 'MACD', 'Volatility', 'EMA20', 'EMA50', 'EMA100']
         elif ';' in lines:
             sep = ';'
             len_row = len(lines.split(sep))
             if len_row == 6:
+                #types = dict(Open=np.float64, High=np.float64, Low=np.float64, Close=np.float64)
                 names = ['Time', 'Open', 'High', 'Low', 'Close', '']
 
         for i in tqdm(range(0, nlines, chunksize), desc="Loading data "):
-            df = pd.read_csv(path, header=None, sep=sep, nrows=chunksize, skiprows=i)#, names = names)
+            df = pd.read_csv(path, header=None, sep=sep, nrows=chunksize,
+                skiprows=i, low_memory=True)
             df.columns = names
             if row is not None:
-                row = row.append(df, ignore_index = True)
+                row = row.append(df, ignore_index=True)
             else:
                 row = df.copy(deep=True)
 
@@ -67,15 +73,19 @@ def getStockDataVec(key):
 
         if len_row == 4 and ',' in sep:
             vec = row['ASK'].copy(deep=True)
-            row.drop(row.columns[[0, 1, 3]], axis=1, inplace=True)
+            row.drop(row.columns[[0]], axis=1, inplace=True)
 
         elif len_row == 3 and ',' in sep:
             vec = row['Price'].copy(deep=True)
-            #row.drop(row.columns[[0, 2]], axis=1, inplace=True)
+            #row.drop(row.columns[[0]], axis=1, inplace=True)
+
+        elif len_row == 9 and ',' in sep:
+            vec = row['Price'].copy(deep=True)
+            row.drop(row.columns[[0]], axis=1, inplace=True)
 
         elif len_row == 6 and ';' in sep:
             vec = row['Close'].copy(deep=True)
-            row.drop(row.columns[[0, 1, 2, 3, 5]], axis=1, inplace=True)
+            row.drop(row.columns[[0,5]], axis=1, inplace=True)
 
         return vec, row, time
 
@@ -86,12 +96,21 @@ def sigmoid(x):
 # returns an an n-day state representation ending at time t
 def getState(data, t, n):
         d = t - n + 1
-        tmp = np.asarray(data)
-        block = tmp[d:t + 1] if d >= 0 else np.concatenate([-d * [tmp[0]]] + [tmp[0:t + 1]])
-        res = []
-        for i in range(n - 1):
-            res.append(sigmoid(block[i + 1] - block[i]))
-        return np.array(res)
+        temp = []
+        for col in data.columns:
+            tmp = np.asarray(data[col])
+            block = tmp[d:t + 1] if d >= 0 else np.concatenate([-d * [tmp[0]]] + [tmp[0:t + 1]])
+            res = []
+            for i in range(n - 1):
+                if not "Volume" in col:
+                    res.append(sigmoid(block[i + 1] - block[i]))
+                else:
+                    res.append(block[i])
+            temp.append(res)
+        datas = []
+        for idx in range(len(temp[0])):
+            datas.append([temp[i][idx] for i in range(len(data.columns))])
+        return np.array(datas)
 
 def act_processing(act):
     if act == 1:
